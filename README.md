bba22ef366af5d97e3183b2053bd0075611acb95
Luiz Felipe <luizfelipecastrovb@gmail.com>
2021-11-30 22:25:35 -0300

    Adding files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# beerunner
+
+A simple command line application to automaticly run a [beecrowd](https://beecrowd.com.br/) exercise
+
+
+## Acknowledgements
+
+For more specific information (installation, usage, prerequisites, etc), see the [project's wiki](https://github.com/lurevar/beerunner/wiki)
---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+declare -gr FILE_TO_INSTALL='bin/beerunner'
+
+declare -g INSTALL_PATH='/usr/bin'
+
+# Help function
+function _help {
+    eval "cat << EOF
+Usage: ./$0 OPTION
+A simple script to install beerunner
+Options:
+    l :     Install beerunner localy
+    g :     Install beerunner globaly
+    u :     Uninstall beerunner
+    h :     Show this help message
+EOF"
+}
+
+# Private utility function
+function __is_root {
+    if ! [ "$EUID" -eq 0 ]
+    then
+        return 0
+    fi
+
+    return 1
+}
+
+# Functions for installing and uninstalling
+function _install {
+    if ! __is_root
+    then
+        echo "You need to be root to run this script"
+        exit 1
+    fi
+
+    if [ -x "$INSTALL_PATH" ]
+    then
+        echo "Making the file executable"
+        eval "chmod +x $FILE_TO_INSTALL"
+    fi
+
+    echo "Installing beerunner in $INSTALL_PATH"
+    eval "cp $FILE_TO_INSTALL $INSTALL_PATH"
+    echo "beerunner was installed successfully"
+}
+
+function _uninstall {
+    if ! __is_root
+    then
+        echo "You need to be root to run this script"
+        exit 1
+    fi
+
+    if [ -z "$INSTALL_PATH" ]
+    then
+        echo "beerunner is not installed"
+        exit 1
+    fi
+
+    echo "Uninstalling beerunner from $INSTALL_PATH"
+    eval "rm -f $INSTALL_PATH"
+    echo "beerunner was unistalled successfully"
+}
+
+# Testing arguments
+if [ "$#" -eq 0 ]
+then
+    echo "Missing option argument (try --help)"
+    exit 1
+fi
+
+# Main of the program
+case "$1" in
+    'l') INSTALL_PATH='/usr/local/bin' _install ;;
+    'g') INSTALL_PATH='/usr/bin' _install ;;
+    'u') INSTALL_PATH=$(which beerunner) _uninstall ;;
+    'h') _help ; exit 0
+esac
